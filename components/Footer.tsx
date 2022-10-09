986b61c1a5967f4c11ebcba7619dafb3cd4973d1
kuratedao <91306375+CurateDAO@users.noreply.github.com>
2022-10-09 13:39:07 -0500

    tightening things up a little bit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div>
-                Backed by Polychain Capital, Avalanche Blizzard Fund, DAO5, and a curated set of angel investors.
+                Backed by Polychain Capital, Avalanche&apos;s Blizzard Fund, DAO5, and a curated set of angel investors.
                 Made by <Link href="https://dbdao.xyz/" target="_blank" rel="noreferrer"><a className={styles.greenlink}>DB DAO</a></Link> in CA, NY, and Berlin.
             </div>
             <div>
